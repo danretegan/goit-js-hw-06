93ba0d2c0e86fd5e1eba645990f7653bf0249e14
Dan <danretegan@yahoo.com>
2023-10-21 18:34:25 +0300

    task-07

---
+++ js/task-07.js
@@ -0,0 +1,7 @@
+const fontSizeControl = document.getElementById("font-size-control");
+
+const text = document.getElementById("text");
+
+fontSizeControl.addEventListener("input", function () {
+  text.style.fontSize = fontSizeControl.value + "px";
+});
