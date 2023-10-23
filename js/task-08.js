24ec0ab5b431bf506d7d9c4efd51782f2b56aed8
Dan <danretegan@yahoo.com>
2023-10-23 08:19:41 +0300

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const loginForm = document.getElementsByClassName("login-form")[0];
 
+loginForm.addEventListener("submit", function (event) {
+  event.preventDefault(); // Evită reîncărcarea paginii la trimiterea formularului.
+
+  const dataForm = {}; //inițializam un obiect gol numit dataForm.
+  const email = document.getElementsByName("email")[0];
+  const password = document.getElementsByName("password")[0];
+
+  if (email.value.trim() === "" || password.value.trim() === "") {
+    alert("Toate câmpurile trebuie completate");
+    return;
+  }
+
+  dataForm["email"] = email.value;
+  dataForm["password"] = password.value;
+
+  console.log(dataForm); // Afișează obiectul cu datele introduse în consolă.
+  loginForm.reset(); // Resetează valorile câmpurilor din formular.
+});
