e047b2636a02df598debd097ee3bfed69fe81d6e
Dan <danretegan@yahoo.com>
2023-10-23 10:16:14 +0300

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const button = document.querySelector(".change-color");
+const colorSpan = document.querySelector(".color");
+
+button.addEventListener("click", function () {
+  // Se generează o culoare aleatoare folosind funcția getRandomHexColor():
+  const randomColor = getRandomHexColor();
+
+  document.body.style.backgroundColor = randomColor; // Se actualizează stilul de fundal al întregii pagini (reprezentată de document.body) cu culoarea generată aleator.
+
+  colorSpan.textContent = randomColor; // Se actualizează textul din interiorul elementului span cu clasa "color" cu valoarea culorii generate aleator:
+});
